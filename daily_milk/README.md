542710535a97dd7cfc4fdbdaa3a7f53345f50737
eagle <keshavamuraari@gmail.com>
2022-10-25 17:37:05 +0530

    home page form w/o db

--- daily_milk/README.md
+++ daily_milk/README.md
@@ -1,16 +1,11 @@
 # daily_milk
 
-A new Flutter project.
+A project to add entry of milk daily bought from local dairy.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+**Internationalization Codegen**
+`flutter gen-l10n`
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+**Generate Adapter**
+`flutter packages pub run build_runner build`
--- daily_milk/lib/pages/ui/home/home_page.dart
+++ daily_milk/lib/pages/ui/home/home_page.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Text('Home Page'),
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          SizedBox(width: MediaQuery.of(context).size.width),
+          SizedBox(
+            width: 120,
+            child: TextFormField(
+              validator: ((value) {
+                return (value?.isEmpty ?? true)
+                    ? AppLocalizations.of(context)!.required
+                    : null;
+              }),
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                suffixText: AppLocalizations.of(context)!.milkQuantitySymbol,
+                hintText: AppLocalizations.of(context)!.milkQuantity,
+                labelText: AppLocalizations.of(context)!.milkLabel,
+                border: const OutlineInputBorder(),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                      content: Text(
+                          AppLocalizations.of(context)!.milkSubmitSuccess)),
+                );
+              }
+            },
+            child: Text(AppLocalizations.of(context)!.milkSubmit),
+          ),
+        ],
+      ),
     );
   }
 }

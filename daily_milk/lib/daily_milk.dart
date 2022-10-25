2178f1a559a26f40b1ac50001c90a72a09380375
eagle <keshavamuraari@gmail.com>
2022-10-25 16:32:21 +0530

    file arrangement

--- daily_milk/lib/daily_milk.dart
+++ daily_milk/lib/daily_milk.dart
@@ -1,4 +1,4 @@
-import 'package:daily_milk/landing_page.dart';
+import 'package:daily_milk/pages/ui/landing_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 

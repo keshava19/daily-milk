d5060c287f7c5a682ad8091c3e678957b73d253a
eagle <keshavamuraari@gmail.com>
2022-10-25 17:24:41 +0530

    naming fix

--- daily_milk/lib/main.dart
+++ daily_milk/lib/main.dart
@@ -4,11 +4,11 @@ import 'package:daily_milk/daily_milk.dart';
 import 'package:daily_milk/model/milk_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as pathProvider;
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Directory directory = await pathProvider.getApplicationDocumentsDirectory();
+  Directory directory = await path_provider.getApplicationDocumentsDirectory();
   Hive.init(directory.path);
   Hive.registerAdapter(MilkModelAdapter());
   await Hive.openBox('hive_box');
